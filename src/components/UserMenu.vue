e71123e9294b07a25e58f0f6a845a1254c72f1f7
stLucky <asys4759@gmail.com>
2022-06-15 20:50:20 +0300

    adds adaptive for home page

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -2,13 +2,28 @@
   <template v-if="!authStore.isAuthorized">
     <button
       type="button"
-      class="border-2 border-white mr-2 hover:border-gray-400 transition-all px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
+      class="border-2 border-white mr-2 hover:border-gray-400  transition-all px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
       id="user-menu-button"
       aria-expanded="false"
       aria-haspopup="true"
       @click="showRegisterModal"
     >
-      Регистрация
+      <span class="hidden sm:block"> Регистрация </span>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 sm:hidden"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
+        />
+      </svg>
     </button>
     <button
       type="button"
@@ -18,7 +33,22 @@
       aria-haspopup="true"
       @click="showLoginModal"
     >
-      Войти
+      <span class="hidden sm:block"> Войти </span>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 sm:hidden"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+        />
+      </svg>
     </button>
   </template>
   <div class="flex items-center" v-else>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,44 @@
 <template>
   <main class="grow">
     <div class="max-w-7xl px-2 sm:px-6 lg:px-8 mx-auto h-full">
-      <div class="flex justify-between items-center h-full">
-        <p class="text-3xl font-bold">
-          У вас запланирована встреча 07.07. 17:00?
-        </p>
-        <ul class="relative">
+      <div
+        class="flex sm:flex-col justify-center items-center content-center h-full flex-wrap text-center"
+      >
+        <ul class="relative mb-8">
           <li
-            class="rounded-xl overflow-hidden max-w-xs shadow-2xl relative z-10"
+            class="rounded-[50%] sm:rounded-xl overflow-hidden max-w-xs shadow-2xl relative z-10 w-52 md:w-72"
           >
             <img src="@/assets/img/place-0.jpg" alt="Москва нулевой километр" />
-            <p class="bg-slate-100 p-8 text-black font-medium text-center">
+            <p
+              class="bg-slate-100 p-4 md:p-8 text-black font-medium text-center hidden sm:block"
+            >
               Москва нулевой километр
             </p>
           </li>
           <li
-            class="rounded-xl overflow-hidden max-w-xs shadow-2xl absolute top-0 left-0 origin-right -translate-y-20 rotate-12"
+            class="rounded-xl overflow-hidden max-w-xs shadow-2xl absolute bottom-0 right-0 translate-x-36 rotate-12 w-44 sm:w-52 md:w-72 hidden sm:block"
           >
             <img src="@/assets/img/place-1.jpg" alt="Москва нулевой километр" />
-            <p class="bg-slate-100 p-8 text-black font-medium text-center">
+            <p
+              class="bg-slate-100 p-4 md:p-8 text-black font-medium text-center"
+            >
               Москва нулевой километр
             </p>
           </li>
           <li
-            class="rounded-xl overflow-hidden max-w-xs shadow-2xl absolute top-0 left-0 origin-bottom-right -translate-x-28 -rotate-12"
+            class="rounded-xl overflow-hidden max-w-xs shadow-2xl absolute bottom-0 left-0 -translate-x-36 -rotate-12 w-44 sm:w-52 md:w-72 hidden sm:block"
           >
             <img src="@/assets/img/place-2.jpg" alt="Москва нулевой километр" />
-            <p class="bg-slate-100 p-8 text-black font-medium text-center">
+            <p
+              class="bg-slate-100 p-4 md:p-8 text-black font-medium text-center"
+            >
               Москва нулевой километр
             </p>
           </li>
         </ul>
+        <p class="font-bold relative z-10 text-lg sm:text-3xl">
+          У вас запланирована встреча 07.07. 17:00?
+        </p>
       </div>
     </div>
   </main>
